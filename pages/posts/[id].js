0536abbcb7a06d5d3bcb878cb76b01327e624f4f
noriyotcp <noriyo.akita@gmail.com>
2023-09-11 01:22:56 +0900

    Display the content of the file

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -12,7 +12,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -28,6 +28,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
     </Layout>
   )
 }
